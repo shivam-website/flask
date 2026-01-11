3a854810f0fed46da38b9abe551a253907bccd3c
Shivam-website <Shivamsah1222@gmail.com>
2026-01-11 19:04:00 +0545

    Added database.py

--- Chapter 5/Chapter 5-PS/01_problem1.py
+++ Chapter 5/Chapter 5-PS/01_problem1.py
@@ -1,21 +1,20 @@
-from flask import Flask, redirect, url_for,render_template,request,session
+from flask import Flask, redirect, url_for, render_template, request, session
 from datetime import timedelta
 
 app = Flask(__name__)
 app.secret_key = "4545dfhsuidfhs8ufs8h3893%^$&*$(#@fhdskjfdksfnsdkjf8#*#($U(#*)))"
-app.permanent_session_lifetime = timedelta(minutes=1)
+app.permanent_session_lifetime = timedelta(seconds=30)#IT only make hte user logged out if the user stays inactive for the given time 
 
 @app.route("/")
 def home():     
-    return render_template("login.html")
+    return render_template("base.html")
 
 @app.route("/login", methods=["POST","GET"])
 def login():
-    if request.method=="POST":
+    if request.method == "POST":
         session.permanent = True
-        user =request.form.get("nm")
-        password =request.form.get("password")
-        session["user"]=user
+        user = request.form.get("nm")
+        session["user"] = user
         return redirect(url_for("user"))
     else:
         if "user" in session:
@@ -26,14 +25,16 @@ def login():
 def user():
     if "user" in session:
         user = session["user"]
-        return render_template("user.html", user=user)
-    return redirect(url_for("login"))
-
+        session.modified = True   # 🔥 keeps session alive
+        return render_template("user.html",user=user)
+    else:
+        return redirect(url_for("login"))
 
 @app.route("/logout")
 def logout():
-    session.pop("user",None)
+    # session.permanent=True
+    session.pop("user", None)
     return redirect(url_for("login"))
 
 if __name__ == "__main__":
-    app.run(debug =True,port=8000)
+    app.run(debug=True, port=8000)  # 🔥 debug OFF
