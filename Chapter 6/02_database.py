911b4e9d85a267ac36c9e633ca5b555a2864096f
Shivam-website <Shivamsah1222@gmail.com>
2026-01-17 15:43:15 +0545

    Added email sessions

--- Chapter 6/02_database.py
+++ Chapter 6/02_database.py
@@ -1,9 +1,10 @@
 from flask import Flask, redirect, url_for, render_template, request, session,flash
 from datetime import timedelta
+import sqlalchemy
 
 app=Flask(__name__)
 app.secret_key ="232jhdjhsdgfas"
-app.permanent_session_lifetime =timedelta(minutes=1)
+app.permanent_session_lifetime =timedelta(minutes=10)
 
 @app.route("/")
 def home():
@@ -15,7 +16,6 @@ def login():
         user = request.form.get("nm")
         session.permanent = True
         session["user"]=user
-        
         flash("You have been logged in","sucess")
         return redirect(url_for("user"))
         
@@ -25,11 +25,19 @@ def login():
             return redirect(url_for("user"))
         return render_template("login.html")
 
-@app.route("/user")
+@app.route("/user",methods=["POST","GET"])
 def user():
+    email = None
     if "user" in session:
         user =session["user"] 
-        return render_template("index.html",user=user)
+        if request.method== "POST":
+            email=request.form.get("email")
+            session["email"] = email
+
+        else:
+            if "email" in session:
+                email = session["email"]
+        return render_template("index.html",email=email,user=user)
     else:
         flash("You are not logged in","info")
         return redirect(url_for("login"))
@@ -37,6 +45,7 @@ def user():
 @app.route("/logout")
 def logout():
     session.pop("user",None)
+    session.pop("email",None)
     flash(f"You have been logged out","info")
     return redirect(url_for("login"))
 
